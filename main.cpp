fa4df45efdfd4f09c5dc3960bd55035425a13a10
Emerson Knapp <emerson@43layers.com>
2016-09-20 13:32:31 -0700

    Calculate and print some basic mesh stats, bbox and volume

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <limits>
+#include <algorithm>
 
 #include <assimp/Importer.hpp> // C++ importer interface
 #include <assimp/Exporter.hpp>
@@ -11,16 +13,84 @@ struct ProgOpts {
   bool printFormats = false;
   const char * outFile = nullptr;
   const char * inFile = nullptr;
-
 };
 
-// void printMeshStates(const aiMesh* mesh, bool doBBox) {
+static const double FLOAT_MAX = std::numeric_limits<float>::max();
+static const double FLOAT_MIN = std::numeric_limits<float>::min();
+
+struct BBox {
+  float minX = FLOAT_MAX;
+  float maxX = FLOAT_MIN;
+
+  float minY = FLOAT_MAX;
+  float maxY = FLOAT_MIN;
+
+  float minZ = FLOAT_MAX;
+  float maxZ = FLOAT_MIN;
+};
+
+// void printMeshStats(const aiMesh* mesh, bool doBBox) {
 //
 // }
 //
-// void printSceneStats(const aiScene* scene) {
-//
-// }
+
+float calculateFaceVolume(const aiVector3D& a, const aiVector3D& b, const aiVector3D& c) {
+  return (
+    a.x*b.y*c.z +
+    a.y*b.z*c.x +
+    a.z*b.x*c.y -
+    a.x*b.z*c.y -
+    a.y*b.x*c.z -
+    a.z*b.y*c.x) / 6;
+}
+
+float calculateMeshVolume(const aiMesh* pMesh) {
+  float volume = 0;
+  for (size_t i = 0; i < pMesh->mNumFaces; i++) {
+    const aiFace& face = pMesh->mFaces[i];
+    if (face.mNumIndices != 3) {
+      printf("Encountered non-triangle face\n");
+      abort();
+    }
+    volume += calculateFaceVolume(
+      pMesh->mVertices[face.mIndices[0]],
+      pMesh->mVertices[face.mIndices[1]],
+      pMesh->mVertices[face.mIndices[2]]
+    );
+  }
+  return volume;
+}
+
+
+BBox calculateBBox(const aiMesh* mesh) {
+    BBox out;
+    const size_t numVerts = mesh->mNumVertices;
+    for (size_t i = 0; i < numVerts; i++) {
+      const aiVector3D& v = mesh->mVertices[i];
+      out.minX = std::min(out.minX, v.x);
+      out.maxX = std::max(out.maxX, v.x);
+      out.minY = std::min(out.minY, v.y);
+      out.maxY = std::max(out.maxY, v.y);
+      out.minZ = std::min(out.minZ, v.z);
+      out.maxZ = std::max(out.maxZ, v.z);
+    }
+    return out;
+}
+
+void printSceneStats(const aiScene* scene) {
+  printf("There are %d meshes in the scene\n", scene->mNumMeshes);
+  for (size_t i = 0; i < scene->mNumMeshes; i++) {
+      const aiMesh* mesh = scene->mMeshes[i];
+      printf("Mesh %zu has %d faces\n", i, mesh->mNumFaces);
+      BBox bb = calculateBBox(mesh);
+      printf("BBox (%f, %f, %f)  (%f, %f, %f)\n", bb.minX, bb.minY, bb.minZ, bb.maxX, bb.maxY, bb.maxZ);
+      printf("X %f\n", bb.maxX - bb.minX);
+      printf("Y %f\n", bb.maxY - bb.minY);
+      printf("Z %f\n", bb.maxZ - bb.minZ);
+      float volume = calculateMeshVolume(mesh);
+      printf("Volume %f\n", volume);
+  }
+}
 
 ProgOpts readOpts(int argc, char** argv) {
   int c;
@@ -52,7 +122,14 @@ ProgOpts readOpts(int argc, char** argv) {
   return out;
 }
 
-
+void printFormats(const Assimp::Exporter& exporter) {
+  size_t exportFormatCount = exporter.GetExportFormatCount();
+  printf("There are %zu export formats available\n", exportFormatCount);
+  for (size_t i = 0; i < exportFormatCount; i++) {
+    const aiExportFormatDesc* pDesc = exporter.GetExportFormatDescription(i);
+    printf("%zu - %s (.%s)\n", i, pDesc->description, pDesc->fileExtension);
+  }
+}
 
 int main(int argc, char** argv) {
     ProgOpts opts = readOpts(argc, argv);
@@ -60,34 +137,26 @@ int main(int argc, char** argv) {
     Assimp::Exporter::Exporter exporter;
 
     if (opts.printFormats) {
-      size_t exportFormatCount = exporter.GetExportFormatCount();
-      printf("There are %zu export formats available\n", exportFormatCount);
-      for (size_t i = 0; i < exportFormatCount; i++) {
-        const aiExportFormatDesc* pDesc = exporter.GetExportFormatDescription(i);
-        printf("%zu - %s (.%s)\n", i, pDesc->description, pDesc->fileExtension);
-      }
+      printFormats(exporter);
       return 0;
     }
-
     if (!opts.inFile) {
       fprintf(stderr, "No input file specified\n");
       return 1;
     }
+
+    // Read file
     const std::string& pFile(opts.inFile);
+    //TODO: aiProcess::Triangulate
     const aiScene* scene = importer.ReadFile( pFile, 0 );
-    // If the import failed, report it
-    if( !scene)
-    {
+    if (!scene) {
         printf("%s\n", importer.GetErrorString());
         return 1;
     }
 
-    printf("There are %d meshes in the scene\n", scene->mNumMeshes);
-    for (size_t i = 0; i < scene->mNumMeshes; i++) {
-        const aiMesh* mesh = scene->mMeshes[i];
-        printf("Mesh %zu has %d faces\n", i, mesh->mNumFaces);
-    }
+    printSceneStats(scene);
 
+    // Export if outfile specified
     if (opts.outFile) {
       const aiExportFormatDesc* pDesc = exporter.GetExportFormatDescription(3);
       exporter.Export(scene, pDesc->id, opts.outFile);
